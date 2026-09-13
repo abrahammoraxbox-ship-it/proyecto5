4e8b65da78a4267777c544f9acd70cd3e7fa911b
abrahammorarr-ai <abrahammorarr@gmail.com>
2026-09-13 14:20:14 -0300

    segundo commit

--- proyecto5/wsgi.py
+++ proyecto5/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for proyecto5 project.
+WSGI config for proyectoweb project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyecto5.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyectoweb.settings')
 
 application = get_wsgi_application()
